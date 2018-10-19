e5f7673badd83e542d92bfbcf60aef2a79bc9ac3
Faraj Farook <farajfarook@gmail.com>
2018-10-20 01:45:31 +1100

    Change version

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of PortProxy",
 	Long:  `All software has versions. This is PortProxy's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("PortProxy v0.0.1 - beta")
+		fmt.Println("PortProxy v0.1.0")
 	},
 }
 
